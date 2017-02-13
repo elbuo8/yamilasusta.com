56937513dc23a919b110f57c74e22a93bb7923c6
Yamil Asusta <hello@yamilasusta.com>
2017-02-13 01:26:34 -0400

    Add env var

--- deployment/deploy-master.sh
+++ deployment/deploy-master.sh
@@ -1,5 +1,5 @@
 npm i -g now json
-now deploy -t $ZEIT_TOKEN --docker
+now deploy -t $ZEIT_TOKEN -e NODE_ENV=production --docker
 DEPLOYMENT_INFO=$(curl -H "Authorization: Bearer $ZEIT_TOKEN" https://api.zeit.co/now/deployments | json -c "console.log(JSON.stringify(this.deployments.filter((e) => {return e.name === '$CIRCLE_PROJECT_REPONAME'}).reduce((a, b) => {return a.created > b.created ? a : b})))")
 echo "running simple check"
 curl -i -L $(echo $DEPLOYMENT_INFO | json url)
