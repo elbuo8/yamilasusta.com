0163eec3dbe507bb68b819763ea9264a90d55234
Yamil Asusta <hello@yamilasusta.com>
2020-08-29 13:54:02 -0400

    Cleanup

--- deployment/deploy-master.sh
+++
@@ -1,5 +0,0 @@
-npm i -g now@4.7.0
-URL=$(now deploy -t $ZEIT_TOKEN -e NODE_ENV=production --docker)
-echo "running simple check on $URL"
-curl --silent -i -L $URL
-now alias set $URL www.yamilasusta.com -t $ZEIT_TOKEN
--- server.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const app = require('./app');
-
-http.createServer(app).listen(process.env.PORT || 3000);
-
-console.log('Server running on', process.env.PORT || 3000);
